475e9f0bd6516fa22c23a18632cd5b7c335a9e05
VTran <MeeMofu@users.noreply.github.com>
2020-09-05 18:01:48 -0400

    local memory storage

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,28 @@
 
-var stamp = ['09:00am','10:00am','11:00am','12:00pm','01:00pm','02:00pm','03:00pm','04:00pm','05:00pm'];
+var eventList;
 
-// Generate the empty fields
-for (var i=0;i<stamp.length;i++){
-    // The html was tested out and parse into a string
-    var field = $("<div>").html("<div class='col-3 col-md-2 time'><span class='mx-auto'>"+stamp[i]+"</span></div><div class='col-9 col-md-10 event'><p></p></div><div class='col-2 col-md-1 d-flex p-0'><button type='button' class='close text-primary clearBtn' aria-label='Close'><span aria-hidden='true'>&times;</span></button><button type='button' class='btn btn-info saveBtn w-100'>Save</button></div>");
-    field.addClass("row no-gutter m-2 h-100").attr("data-field",i);
+var createFields = function(){
+    var stamp = ['09:00am','10:00am','11:00am','12:00pm','01:00pm','02:00pm','03:00pm','04:00pm','05:00pm'];
     
-    // field.find(".event p").text("Test");
-
-    // Added the element to container
-    $(".container").append(field);
-    $("button").hide();
+    eventList = JSON.parse(localStorage.getItem("eventList"));
+    if (!eventList){
+        eventList = Array(stamp.length).fill(' ');
+    }
+    // Generate the empty fields
+    for (var i=0;i<stamp.length;i++){
+        // The html was tested out and parse into a string
+        var field = $("<div>").html("<div class='col-3 col-md-2 time'><span class='mx-auto'>"+stamp[i]+"</span></div><div class='col-9 col-md-10 event'><p></p></div><div class='col-2 col-md-1 d-flex p-0'><button type='button' class='close text-primary clearBtn' aria-label='Close'><span aria-hidden='true'>&times;</span></button><button type='button' class='btn btn-info saveBtn w-100'>Save</button></div>");
+        field.addClass("row no-gutter m-2 h-100").attr("data-field",i);
+        
+        // Updated the field with saved event
+        field.find(".event p").text(eventList[i]);
+    
+        // Added the element to container
+        $(".container").append(field);
+        $("button").hide();
+    }
 }
+
 
 // Start editing a field
 $(".container").on("click", ".event",function(){
@@ -58,7 +68,10 @@ $(".container").on("click", ".event",function(){
 // Save the updated field
 var save = function(rowEl){
     var id = rowEl.attr("data-field");
-    console.log("save "+id);
+    eventList[id]= rowEl.find("p").text().trim();
+
+    // save to local storage
+    localStorage.setItem("eventList", JSON.stringify(eventList));
 }
 
 // Reset the field into previous state
@@ -98,3 +111,5 @@ $(".container").on("click",".clearBtn",function(){
     $(this).closest(".row").find("textarea").val("")
     reset($(this).closest(".row"));
 });
+
+createFields();
