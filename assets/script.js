5f8e3cf437a7f3260f1ee6f42584b790f12537d0
VTran <MeeMofu@users.noreply.github.com>
2020-09-05 13:56:59 -0400

    Added button functionality

--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,69 @@
 $("button").hide();
 $(".container").on("click", ".event",function(){
     var text = $(this).find("p")
-    .text()
-    .trim();
-    
-        
-    // console.log($(this).closest(".row").attr("data-field"));
+        .text()
+        .trim();
+    var rowEl=$(this).closest(".row");
     $(this).switchClass(
         "col-9 col-md-10",
         "col-7 col-md-9",
         {
             duration:200,
             complete: function(){
-                $(this).closest(".row").find(".btn").show({
+                rowEl.find(".btn").show({
                     effect:"slide",
                     duration:100,
                     complete: function(){
-                        $(this).closest(".row").find(".clearBtn").show();
+                        rowEl.find(".clearBtn").show();
                         var textInput = $("<textarea>")
                             .addClass("form-control mr-3 my-2")
-                            .attr("rows",4)
+                            .attr("rows",2)
                             .val(text);
-                        $(this).closest(".row").find(".event p").replaceWith(textInput);
+                        rowEl.find(".event p").replaceWith(textInput);
                         textInput.trigger("focus");
-                        console.log(this)
                     }
                 })
             }
         })
         
 });
-$(".container").on("click",".btn",function(){
-    var id = $(this).closest(".row").attr("data-field");
-    console.log(id);
-});
-$(".container").on("click",".clearBtn",function(){
-    var id = $(this).closest(".row").attr("data-field");
-    console.log("clear"+id);
-});
-$(".container").on("blur","textarea",function(){
-    var text = $(this)
+// $(".container").on("blur","textarea",function(){
+//     var text = $(this)
+//     .val()
+//     .trim();
+//     var newEvent = $("<p>")
+//     .text(text);
+//     $(this).replaceWith(newEvent);
+// });
+
+var reset = function(rowEl){
+    rowEl.find(".btn").hide({
+        effect: "slide",
+        duration: 100,
+        complete: function(){
+            rowEl.find(".clearBtn").hide();
+            rowEl.find(".event").switchClass(
+                "col-7 col-md-9",
+                "col-9 col-md-10"
+            )
+        }
+    })
+    var text = rowEl.find("textarea")
         .val()
         .trim();
     var newEvent = $("<p>")
-        .text(text);
-    console.log(this);
-      // replace textarea with p element
-      $(this).replaceWith(newEvent);
-    // var newEvent = $("<span>")
-    //     .text(text);
-    // $(this).replaceWith(newEvent);
+    .text(text);
+    rowEl.find("textarea").replaceWith(newEvent);
+}
+
+$(".container").on("click",".btn",function(){
+    var id = $(this).closest(".row").attr("data-field");
+    console.log("save "+id);
+    reset($(this).closest(".row"));
+});
+$(".container").on("click",".clearBtn",function(){
+    var id = $(this).closest(".row").attr("data-field");
+    $(this).closest(".row").find("textarea").val("")
+    console.log("clear "+id);
+    reset($(this).closest(".row"));
 });
